a26b89020276813161a53ebfcb8c6288b12420ef
patryku <patryk.u93@gmail.com>
2017-05-21 23:45:03 +0200

    Route recording implemented

--- AtlasClient/android/app/src/main/java/com/atlasclient/MainApplication.java
+++ AtlasClient/android/app/src/main/java/com/atlasclient/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.rnfs.RNFSPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new MapsPackage(),
-          new KCKeepAwakePackage()
+          new KCKeepAwakePackage(),
+          new RNFSPackage()
       );
     }
   };
--- AtlasClient/index.android.js
+++ AtlasClient/index.android.js
@@ -3,6 +3,8 @@ import { Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native
 import MapView from 'react-native-maps';
 import KeepAwake from 'react-native-keep-awake';
 
+var RNFS = require('react-native-fs');
+
 export default class AtlasClient extends Component {
 
   constructor(props) {
@@ -18,6 +20,15 @@ export default class AtlasClient extends Component {
         latitude: 50.030521,
         longitude: 19.907176,
       },
+      route: [
+        { latitude: 50.030303, longitude: 19.907702 },
+        { latitude: 50.031696, longitude: 19.909246 },
+        { latitude: 50.031848, longitude: 19.908924 },
+        { latitude: 50.031703, longitude: 19.908291 },
+        { latitude: 50.032054, longitude: 19.906918 },
+        { latitude: 50.033033, longitude: 19.907637 },
+      ],
+      recordTitle: 'Record route',
     };
   }
 
@@ -47,28 +58,57 @@ export default class AtlasClient extends Component {
   };
 
   onRecordPress() {
-    Alert.alert('Record has been pressed!');
+    if (this.state.recordTitle == 'Record route') {
+      this.recordedRoute = [];
+      this.watchID = navigator.geolocation.watchPosition(
+        (position) => {
+          this.recordedRoute.push({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+          this.setState({
+            region: {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            },
+            userPosition: {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            },
+            route: this.recordedRoute.slice(),
+          });
+        },
+        (error) => {},
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+      );
+      this.setState({ route: this.recordedRoute.slice(), recordTitle: 'Save route' });
+    } else {
+      navigator.geolocation.clearWatch(this.watchID);
+      var path = RNFS.ExternalDirectoryPath + '/AtlasRoute.json';
+      RNFS.writeFile(path, JSON.stringify(this.recordedRoute), 'utf8')
+      .then((success) => {
+        alert('The route has been saved.');
+      })
+      .catch((err) => {
+        alert('Unable to save the route.');
+      });
+      this.setState({ route: this.recordedRoute.slice(), recordTitle: 'Record route' });
+    }
   };
 
   render() {
-    let route = [
-      { latitude: 50.030303, longitude: 19.907702 },
-      { latitude: 50.031696, longitude: 19.909246 },
-      { latitude: 50.031848, longitude: 19.908924 },
-      { latitude: 50.031703, longitude: 19.908291 },
-      { latitude: 50.032054, longitude: 19.906918 },
-      { latitude: 50.033033, longitude: 19.907637 },
-    ];
     return (
       <View style={styles.parent}>
         <MapView style={styles.map} region={this.state.region}>
           <MapView.Marker coordinate={this.state.userPosition}/>
-          <MapView.Polyline coordinates={route} strokeWidth={3} strokeColor='blue'/>
+          <MapView.Polyline coordinates={this.state.route} strokeWidth={3} strokeColor='blue'/>
         </MapView>
         <View style={styles.buttons}>
           <Button title='Display route' onPress={this.onDisplayPress.bind(this)} color='blue'/>
           <Button title='My location' onPress={this.onLocationPress.bind(this)} color='green'/>
-          <Button title='Record route' onPress={this.onRecordPress.bind(this)} color='red'/>
+          <Button title={this.state.recordTitle} onPress={this.onRecordPress.bind(this)} color='red'/>
         </View>
         <KeepAwake/>
       </View>
