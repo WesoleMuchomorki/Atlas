d6490ebae78c10cc798a0a7f8a79d9cbea33c964
Muchomor Grabarz <mz18@o2.pl>
2017-06-12 02:35:24 +0200

    List routes

--- AtlasServer/server.go
+++ AtlasServer/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
+	"encoding/json"
 )
 
 //
@@ -17,6 +18,22 @@ func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//
+// Structs
+//
+
+type point struct {
+	ord int
+	Latitude float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type route struct {
+	User string `json:"user"`
+	Id int `json:"id"`
+	Name string `json:"name"`
 }
 
 //
@@ -45,10 +62,28 @@ func getRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "[{\"latitude\":50.050,\"longitude\":19.936},{\"latitude\":50.053,\"longitude\":19.937},{\"latitude\":50.055,\"longitude\":19.934},{\"latitude\":50.061,\"longitude\":19.932},{\"latitude\":50.064,\"longitude\":19.931},{\"latitude\":50.068,\"longitude\":19.926},{\"latitude\":50.071,\"longitude\":19.920}]")
 }
 
-func getRouteList(w http.ResponseWriter, r *http.Request) {
+func getRouteList(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	// XXX placeholder
-	fmt.Fprintf(w, "[\"placeholder 1\",\"placeholder_2\"]")
+
+	rows, err := db.Query("SELECT trips.id, trips.name, users.name FROM trips JOIN users ON trips.user_id = users.id")
+
+	checkErr(err)
+
+	var list []route
+
+	for rows.Next() {
+		var t route
+
+		err = rows.Scan(&t.Id, &t.Name, &t.User)
+		checkErr(err)
+
+		list = append(list,t)
+	}
+
+	json_list, _ := json.Marshal(list)
+
+	fmt.Println(string(json_list))
+	fmt.Fprintf(w, string(json_list))
 }
 
 //
@@ -63,7 +98,7 @@ func main() {
 	r.HandleFunc("/", index).Methods("GET")
 	r.HandleFunc("/routes/{id}", putRoute).Methods("PUT")
 	r.HandleFunc("/routes/{id}", getRoute).Methods("GET")
-	r.HandleFunc("/routes/", getRouteList).Methods("GET")
+	r.HandleFunc("/routes/", func(w http.ResponseWriter, r *http.Request) {getRouteList(db,w,r)})
 	http.ListenAndServe(":8000", r)
 
 	db.Close()
